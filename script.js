be56199bd07cd3940e66ca2acffe0f4c3d25018e
Mathew Theberge <140298808+Mathew-Theberge@users.noreply.github.com>
2024-08-28 17:34:20 -0400

    add gameboard array

---
+++ script.js
@@ -0,0 +1,17 @@
+const Gameboard = (function () {
+    const rows = 3
+    const columns = 3
+    const board = []
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = []
+        for (let j = 0; j < columns; j++) {
+            board[i].push(0)
+        }
+    }
+
+    return {board}
+})()
+
+console.log(Gameboard.board)
+    
