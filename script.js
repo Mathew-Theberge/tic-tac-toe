52c1d6099c32da03138d48f4bcb3fdc4596ee51b
Mathew Theberge <140298808+Mathew-Theberge@users.noreply.github.com>
2024-08-29 17:12:12 -0400

    add way to change cell value using private varibles

--- script.js
+++ script.js
@@ -6,12 +6,30 @@ const Gameboard = (function () {
     for (let i = 0; i < rows; i++) {
         board[i] = []
         for (let j = 0; j < columns; j++) {
-            board[i].push(0)
+            board[i].push(Cell())
         }
     }
-
+    
     return {board}
+
+    function Cell() {
+        let value = 0;
+      
+        // Accept a player's token to change the value of the cell
+        const addToken = (player) => {
+          value = player;
+        };
+      
+        // How we will retrieve the current value of this cell through closure
+        const getValue = () => value;
+      
+        return {
+          addToken,
+          getValue
+        };
+      }
+
 })()
 
 console.log(Gameboard.board)
-    
+
