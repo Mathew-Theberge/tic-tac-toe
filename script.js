9c5f1ec28753e55eb4724c7c3360419016118b5e
Mathew Theberge <140298808+Mathew-Theberge@users.noreply.github.com>
2024-08-30 20:56:06 -0400

    make gamecontroller module add droptoken and switchturn functions
    also working on isgamewon function i felt i needed it
    to figure out how droptoken func would work but the isgamewon
    function is still a work in progress

--- script.js
+++ script.js
@@ -9,8 +9,13 @@ const Gameboard = (function () {
             board[i].push(Cell())
         }
     }
+
+    const printBoard = () => {
+        const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
+        console.log(boardWithCellValues);
+      };
     
-    return {board}
+    return {board, printBoard}
 
     function Cell() {
         let value = 0;
@@ -28,8 +33,70 @@ const Gameboard = (function () {
           getValue
         };
       }
+})()
 
+
+const GameController = (function () {
+    const playerOne = "Player One"
+    const playerTwo = "Player Two"
+
+    const players = [
+        {
+            name: playerOne,
+            token: "X"
+        },
+
+        {
+            name: playerTwo,
+            token: "O"
+        }
+    ]
+    const board = Gameboard.board
+
+    let activePlayer = players[0]
+
+    const switchPlayerTurn = () => {
+        switch(activePlayer) {
+            case players[0]:
+                activePlayer = players[1]
+                break
+            case players[1]:
+                activePlayer = players[0]
+        }
+    }
+
+    const dropToken = (player) => {
+        let userInputRow = prompt("pick row 1-3")
+        let userInputColumn = prompt("pick column 1-3")
+        if (board[userInputRow][userInputColumn].getValue() === 0) {
+            board[userInputRow][userInputColumn].addToken(player.token)
+            if (!isGameWon()) {
+                switchPlayerTurn()
+                Gameboard.printBoard()
+                dropToken(activePlayer)
+            } else {
+                console.log(`${activePlayer.name} won`)
+                Gameboard.printBoard()
+            }
+        }
+    }
+
+    const isGameWon = () => {
+        if (board.some( (row) => {
+            if (row.every( (cell) => {
+                return cell.getValue() === "X" || cell.getValue() === "O"
+            })) {
+                return true
+            }
+        })) {
+            return true
+        }
+    }
+
+    return {switchPlayerTurn, dropToken, activePlayer}
 })()
 
 console.log(Gameboard.board)
+Gameboard.printBoard()
+GameController.dropToken(GameController.activePlayer)
 
